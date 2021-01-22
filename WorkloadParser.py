e6b13e60a1ab1647069ff6816e2d541c91976a10
Geena Smith <geena_smith@hotmail.com>
2021-01-21 18:15:37 -0800

    basic parser with http calls

--- WorkloadParser.py
+++ WorkloadParser.py
@@ -1,12 +1,15 @@
 import sys
+import requests
 
 
 class WorkloadParser:
-    def __init__(self, command_list):
+    def __init__(self, command_list, ip, port):
         """
-        TODO: set up HTTP stuff
         """
         self.commandList = command_list  # (cmd, args)
+        self.ip = ip
+        self.port = port
+        self.host = f'http://{self.ip}:{self.port}'
 
     def run(self):
         for (cmd, args) in self.commandList:
@@ -16,7 +19,7 @@ class WorkloadParser:
                 print(f'Error submitting command {cmd} with params {args}')
 
     def makeCommand(self, cmd, args):
-        print(f'Command: {cmd}: {args}')
+        print(f'Command: {cmd} Arguments: {args}')
 
         if (cmd == 'ADD'):
             self.addRequest(args)
@@ -55,107 +58,159 @@ class WorkloadParser:
 
     def addRequest(self, args):
         """
-        ADD userid,amount --> put?
+        ADD userid,amount --> PUT
+        url: /add
         """
-        pass
+        payload = {'userId': args[0], 'amount': float(args[1])}
+        r = requests.put(f'{self.host}/add', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
     def quoteRequest(self, args):
         """
-        QUOTE userid,StockSymbol --> get
+        QUOTE userid,StockSymbol --> GET
+        url: /quote
         """
-        pass
+        payload = {'userId': args[0], 'stockSymbol': args[1]}
+        r = requests.get(f'{self.host}/quote', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
     def buyRequest(self, args):
         """
-        BUY userid,StockSymbol,amount --> post
+        BUY userid,StockSymbol,amount --> POST
+        url: /buy/create
         """
-        pass
+        payload = {'userId': args[0], 'stockSymbol': args[1], 'amount': float(args[2])}
+        r = requests.post(f'{self.host}/buy/create', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
     def commitBuyRequest(self, args):
         """
-        COMMIT_BUY userid --> post
+        COMMIT_BUY userid --> POST
+        url: /buy/commit
         """
-        pass
+        payload = {'userId': args[0]}
+        r = requests.post(f'{self.host}/buy/commit', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
     def cancelBuyRequest(self, args):
         """
-        CANCEL_BUY userid --> post
+        CANCEL_BUY userid --> POST
+        url: /buy/cancel
         """
-        pass
+        payload = {'userId': args[0]}
+        r = requests.post(f'{self.host}/buy/cancel', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
     def sellRequest(self, args):
         """
-        SELL userid,StockSymbol,amount --> post
+        SELL userid,StockSymbol,amount --> POST
+        url: /sell/create
         """
-        pass
+        payload = {'userId': args[0], 'stockSymbol': args[1], 'amount': float(args[2])}
+        r = requests.post(f'{self.host}/sell/create', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
     def commitSellRequest(self, args):
         """
-        COMMIT_SELL userid --> post
+        COMMIT_SELL userid --> POST
+        url: /sell/commit
         """
-        pass
+        payload = {'userId': args[0]}
+        r = requests.post(f'{self.host}/sell/commit', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
     def cancelSellRequest(self, args):
         """
-        CANCEL_SELL userid --> post
+        CANCEL_SELL userid --> POST
+        url: /sell/cancel
         """
-        pass
+        payload = {'userId': args[0]}
+        r = requests.post(f'{self.host}/sell/cancel', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
     def setBuyAmountRequest(self, args):
         """
-        SET_BUY_AMOUNT userid,StockSymbol,amount --> post
+        SET_BUY_AMOUNT userid,StockSymbol,amount --> POST
+        url: /setBuy/amount
         """
-        pass
+        payload = {'userId': args[0], 'stockSymbol': args[1], 'amount': float(args[2])}
+        r = requests.post(f'{self.host}/setBuy/amount', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
     def cancelSetBuyRequest(self, args):
         """
-        CANCEL_SET_BUY userid,StockSymbol --> post
+        CANCEL_SET_BUY userid,StockSymbol --> POST
+        url: /setBuy/cancel
         """
-        pass
+        payload = {'userId': args[0], 'stockSymbol': args[1]}
+        r = requests.post(f'{self.host}/setBuy/cancel', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
     def setBuyTriggerRequest(self, args):
         """
-        SET_BUY_TRIGGER userid,StockSymbol,amount --> post
+        SET_BUY_TRIGGER userid,StockSymbol,amount --> POST
+        url: /setBuy/trigger
         """
-        pass
+        payload = {'userId': args[0], 'stockSymbol': args[1], 'amount': float(args[2])}
+        r = requests.post(f'{self.host}/setBuy/trigger', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
     def setSellAmountRequest(self, args):
         """
-        SET_SELL_AMOUNT userid,StockSymbol,amount --> post
+        SET_SELL_AMOUNT userid,StockSymbol,amount --> POST
+        url: /setSell/amount
         """
-        pass
+        payload = {'userId': args[0], 'stockSymbol': args[1], 'amount': float(args[2])}
+        r = requests.post(f'{self.host}/setSell/amount', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
     def setSellTriggerRequest(self, args):
         """
-        SET_SELL_TRIGGER userid,StockSymbol,amount --> post
+        SET_SELL_TRIGGER userid,StockSymbol,amount --> POST
+        url: /setSell/trigger
         """
-        pass
+        payload = {'userId': args[0], 'stockSymbol': args[1], 'amount': float(args[2])}
+        r = requests.post(f'{self.host}/setSell/trigger', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
     def cancelSetSellRequest(self, args):
         """
-        CANCEL_SET_SELL userid,StockSymbol --> post
+        CANCEL_SET_SELL userid,StockSymbol --> POST
+        url: /setSell/cancel
         """
-        pass
+        payload = {'userId': args[0], 'stockSymbol': args[1]}
+        r = requests.post(f'{self.host}/setSell/cancel', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
     def dumplogRequest(self, args):
         """
-        DUMPLOG userid,filename --> get
-        DUMPLOG filename --> get
+        TODO: decide how we want to handle the distinction. Right now implementing as userId empty
+        DUMPLOG userid,filename --> GET
+        DUMPLOG filename --> GET
+        url: /dumplog
         """
-        pass
+        if len(args) == 1:
+            payload = {'userId': '', 'fileName': args[0]}
+        else:
+            payload = {'userId': args[0], 'fileName': args[1]}
+        r = requests.get(f'{self.host}/dumplog', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
     def displaySummaryRequest(self, args):
         """
-        DISPLAY_SUMMARY userid --> get
+        DISPLAY_SUMMARY userid --> GET
+        url: /displaySummary
         """
-        pass
+        payload = {'userId': args[0]}
+        r = requests.get(f'{self.host}/displaySummary', params=payload)
+        print(f'Response {r.status_code} at url {r.url}')
 
 
 def parseWorkloadFile(filename):
     command_list = []
     with open(filename, 'r') as f:
         for line in f:
-            split_line = line.split(' ')
+            split_line = line.rstrip().split(' ')
             user_command = split_line[1].split(',')
 
             cmd = user_command[0]
@@ -170,15 +225,18 @@ def callWorkloadParser(args):
     """
     TODO: implement multithreading, error handling
     """
-    if len(args) < 2:
-        print(f'Please provide an input file')
+
+    if len(args) < 4:
+        print(f'Please provide an input file, ip address and port')
         return
+
     filename = args[1]
+    ip = args[2]
+    port = args[3]
 
     command_list = parseWorkloadFile(filename)
-    # print(command_list)
 
-    parser = WorkloadParser(command_list)
+    parser = WorkloadParser(command_list, ip, port)
     parser.run()
 
 
